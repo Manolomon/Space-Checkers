4da958de5597c3e62cfcb9512459d8117bdc617a
Dany HV <dannyhvalenz@live.com.mx>
2018-11-01 09:07:23 -0600

    Chat

---
+++ Space Checkers/ChatSC/Assets/Scripts/ChatListObject.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ChatListObject : MonoBehaviour {
+
+	public Text parentText;
+	public Image chatbarImage;
+	public Text childText;
+    public Text username;
+
+}
